447ea78f4749b91743f2e2136729649f7ab83a21
dubovac <dubovac@users.noreply.github.com>
2022-11-06 11:32:49 -0600

    Create 1588.SumOfAllOddLengthSubarrays.js

---
+++ 1588.SumOfAllOddLengthSubarrays.js
@@ -0,0 +1,72 @@
+/*
+ * 1588. Sum of All Odd Length Subarrays
+ * Easy
+ *
+ * Given an array of positive integers arr, return the sum of all possible odd-length subarrays of arr.
+ *
+ * A subarray is a contiguous subsequence of the array.
+ *
+ * Example 1:
+ *
+ * 	Input: arr = [1,4,2,5,3]
+ * 	Output: 58
+ * 	Explanation: The odd-length subarrays of arr and their sums are:
+ * 	[1] = 1
+ * 	[4] = 4
+ * 	[2] = 2
+ * 	[5] = 5
+ * 	[3] = 3
+ * 	[1,4,2] = 7
+ * 	[4,2,5] = 11
+ * 	[2,5,3] = 10
+ * 	[1,4,2,5,3] = 15
+ * 	If we add all these together we get 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15 = 58
+ *
+ * Example 2:
+ *
+ * 	Input: arr = [1,2]
+ * 	Output: 3
+ * 	Explanation: There are only 2 subarrays of odd length, [1] and [2]. Their sum is 3.
+ *
+ * Example 3:
+ *
+ * 	Input: arr = [10,11,12]
+ * 	Output: 66
+ *
+ * Constraints:
+ *
+ * 	1 <= arr.length <= 100
+ * 	1 <= arr[i] <= 1000
+ */
+
+function sumOddLengthSubarrays(arr) {
+	let length = arr.length
+	let sum = 0;
+
+	for (let i = 1; i <= length; i += 2)
+		for (let j = 0; j <= length - i; j++)
+			for (let k = 0; k < i; k++)
+				sum += arr[k + j];
+
+	return sum;
+}
+
+console.log(sumOddLengthSubarrays([1, 4, 2, 5, 3]));
+console.log(sumOddLengthSubarrays([1, 2]));
+console.log(sumOddLengthSubarrays([10, 11, 12]));
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+
+    Terms
+    Privacy
+    Security
+    Status
+    Docs
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+

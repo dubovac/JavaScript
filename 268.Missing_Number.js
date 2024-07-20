33502a0e8a6c6b2d8520c517d2cebc2e979eb624
dubovac <dubovac@users.noreply.github.com>
2024-07-19 22:34:40 -0500

    Create 268.Missing_Number.js

---
+++ 268.Missing_Number.js
@@ -0,0 +1,68 @@
+/**
+ * 268. Missing Number
+ * Easy
+ * Topics
+ * Companies
+ *
+ * Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * 	Input: nums = [3,0,1]
+ * 	Output: 2
+ * 	Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+ *
+ * Example 2:
+ *
+ * 	Input: nums = [0,1]
+ * 	Output: 2
+ * 	Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+ *
+ * Example 3:
+ *
+ * 	Input: nums = [9,6,4,2,3,5,7,0,1]
+ * 	Output: 8
+ * 	Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9].
+ * 	8 is the missing number in the range since it does not appear in nums.
+ *	
+ *	
+ *
+ * Constraints:
+ *
+ * 	n == nums.length
+ * 	1 <= n <= 104
+ * 	0 <= nums[i] <= n
+ * 	All the numbers of nums are unique.
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+ 	let result = -1;
+
+	for (let i = 0; i <= nums.length; i++) {
+		let j = 0;
+
+		while (j < nums.length) {
+			if (i == nums[j])
+				break;
+			j++;
+		}
+
+		if (j < nums.length)
+			continue;
+		else {
+			result = i;
+			break;
+		}
+	}
+
+	return result;   
+};
+
+console.log(missingNumber([3, 0, 1]));
+console.log(missingNumber([0, 1]));
+console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]));
